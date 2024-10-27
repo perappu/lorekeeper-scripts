e57b7a02f233c8d1fb94f061cbf3f0a3f6aa1f82
perappu <stokoriel@gmail.com>
2024-10-27 18:28:22 -0500

    update setup v3 script

--- bash/setup-v3-local.sh
+++ bash/setup-v3-local.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
-git clone https://github.com/corowne/lorekeeper.git release/v3.0.0 .
+git clone https://github.com/corowne/lorekeeper.git --branch release/v3.0.0 $1
+
+cd $1
 
 git branch -m main
 
 git remote rename origin lorekeeper
 
-git remote add $1 site
+git remote add site $2
 
-git remote add $2 origin
+git remote add origin $3
 
 # add common extension sources
+git remote add ext-CH3RVB-lorekeeper-tweaks https://github.com/CH3RVB/lorekeeper-tweaks.git
 
+git remote add ext-AW0005 https://github.com/AW0005/lorekeeper-extensions.git
+
+git remote add ext-DeeP-ci https://github.com/DeeP-ci/lorekeeper.git
+
+git remote add ext-JuniJwi https://github.com/JuniJwi/lorekeeper.git
+
+git remote add ext-MarskyMessier https://github.com/MarskyMessier/lorekeeper.git
+
+git remote add ext-AnimatedCritter https://github.com/AnimatedCritter/lorekeeper.git 
+
+git remote add ext-CH3RVB https://github.com/CH3RVB/lorekeeper.git
+
+git remote add ext-Cylunny https://github.com/Cylunny/lorekeeper.git
+
+git remote add ext-itinerare https://github.com/itinerare/lorekeeper.git
+
+git remote add ext-lumephobia https://github.com/lumephobia/lorekeeper.git
+
+git remote add ext-perappu https://github.com/perappu/lorekeeper.git
+
+git remote add ext-preimpression https://github.com/preimpression/lorekeeper.git
+
+git remote add ext-Draginraptor https://github.com/Draginraptor/lorekeeper.git
+
+git remote add ext-ScuffedNewt https://github.com/ScuffedNewt/lorekeeper.git
 
 git push origin main
